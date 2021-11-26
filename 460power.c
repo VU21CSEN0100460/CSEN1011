d4297d9517a54ac7c58bb28eea1f97ed1c5ba925
dynamight1 <93367818+dynaamight1@users.noreply.github.com>
2021-11-26 11:47:06 +0530

    Add files via upload

---
+++ 460power.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+ { 
+   int i,j;
+   printf("Enter your base");
+   scanf("%ld",&i);
+   printf("Enter your number");
+   scanf("%ld" ,&j),
+   printf("Your calculted value is is %f",pow(i,j));
+   
+ }
