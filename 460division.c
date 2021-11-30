f164f004076b0c9063f04124f2ec4bd73b75cbb5
dynamight1 <93367818+dynaamight1@users.noreply.github.com>
2021-11-30 14:43:54 +0530

    Add files via upload

---
+++ 460division.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int no,divisor,remainder;
+	
+	printf("Enter the number");
+	scanf("%d", &no);
+	
+	printf("Enter the divisor");
+	scanf("%d" ,&divisor);
+	 
+	while(no >= divisor){
+    no=no-divisor;
+		 
+	}
+}
+
